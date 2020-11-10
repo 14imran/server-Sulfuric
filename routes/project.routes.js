faa434085a25a047cc2d2a68f6850d75506bd1bc
14imran <14.imran.mohammed@gmail.com>
2020-11-10 18:57:00 +0530

    projects(routes crud)

--- routes/project.routes.js
+++ routes/project.routes.js
@@ -34,7 +34,52 @@ router.post('/create',   (req, res) => {
         })  
 })
 
+// api/projects/:myId
 
+router.get('/:myId',  (req, res) => {
+     ProjectModel.findById(req.params.myId)
+       .then((response) => {
+            res.status(200).json(response)
+       })
+       .catch((err) => {
+            res.status(500).json({
+                 error: 'Something went wrong',
+                 message: err
+            })
+       }) 
+  })
+ // api/projects/delete/:id
+ 
+  router.delete('/delete/:id',  (req, res) => {
+   ProjectModel.findByIdAndDelete(req.params.id)
+            .then((response) => {
+                 res.status(200).json(response)
+            })
+            .catch((err) => {
+                 res.status(500).json({
+                      error: 'Something went wrong',
+                      message: err
+                 })
+            })  
+  })
+
+  // api/projects/edit/:id
+router.patch('/edit/:id',  (req, res) => {
+      let id = req.params.id
+      const {projectName, title,startDate,endDate,amount,mark} = req.body;
+   ProjectModel.findByIdAndUpdate(id, {$set: {projectName: projectName, title: title, startDate: startDate,endDate:endDate,amount:amount,mark:mark}})
+            .then((response) => {
+                 res.status(200).json(response)
+            })
+            .catch((err) => {
+                 console.log(err)
+                 res.status(500).json({
+                      error: 'Something went wrong',
+                      message: err
+                 })
+            }) 
+  })
+ 
 
 
 module.exports = router;
